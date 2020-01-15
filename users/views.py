03f680a9ee11845774aa1ef05a6e85a512ec640b
samudsan2020@gmail.com <samudsan2020@gmail.com>
2020-01-15 20:22:26 +0530

    login-logout added

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -10,9 +11,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, "Account created for "+username)
-            return redirect('blog-home')
+            messages.success(request, "your account has been created, you can login now.")
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 

a76e39e14ad4878d602689d72bea363637c16d05
samudsan2020@gmail.com <samudsan2020@gmail.com>
2020-01-19 14:42:58 +0530

    paginated home

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views # . represents current directory
-from .views import PostListView,PostDetailedView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import (
+    PostListView,
+    PostDetailedView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+)
 
 
 urlpatterns = [
     # path('admin/', admin.site.urls), #this is if you want to show admin page
     #for showing home page
     path('', PostListView.as_view(), name='blog-home'),
+    path('post/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailedView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
     path('about/', views.about, name='blog-about'),
 ]
